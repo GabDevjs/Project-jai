7cebdc321fb853c5647f1a6c0dc8530f739de448
Gabriel <flaviogabrielsr0507@gmail.com>
2022-05-15 18:57:05 -0300

    initial commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,41 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
     ProductsContainer,
     ImageConatiner,
+    Image,
+    ImagemItem,
+    SemelantyContainer,
+    ScrollConatiner,
+    SearchContainer,
+    SearchIcon,
+    ContainerItems,
 } from "./styles";
 
+//icons
+import {
+    AiOutlineSearch,
+} from "react-icons/ai";
+
 const Products = () => {
-    const [image, setImage] = useState("");
-    const clientId = "412862363959448ea131ec1e9cd400e3";
-    const [result, setResult] = useState([]);
-    const handleChange = (event) => {
-        setImage(event.target.value);
-    };
-    const handleSubmit = () => {
-        console.log(image);
-        const url = `https://myceliademo.blob.core.windows.net/fashion-imgs/images/Auth_Key: ${clientId} `;
-        axios.get(url).then((response) => {
-            console.log(response);
-            setResult(response.data.results);
-        });
+    const [idtext, setIdtext] = useState("10000");
+    const [results, setResults] = useState([]);
+
+    var config = {
+        method: 'get',
+        url: `https://mycelia.azure-api.net/similar/id/productimages?id=${idtext}&top_k=15`,
+        headers: {
+            'Auth': '412862363959448ea131ec1e9cd400e3',
+            'Content-Type': 'application/json'
+        }
     };
 
+    useEffect(() => {
+        setResults([]);
+        axios(config)
+            .then(function (response) {
+                setResults(response.data.similarity[0].results);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [idtext]);
+
+    console.log(results);
     return (
         <ProductsContainer>
             <ImageConatiner>
-                <input onChange={handleChange} type="text" name="image" placeholder="Search for images" />
-                <button onClick={handleSubmit} type="submit">Search</button>
-                {result.map((image) => (
-                    <>
-                        <div>
-                            <img src={image.urls.thumb} />
-                            <p> Photo by {image.user.name}</p>
-                            <p>👍 {image.likes}</p>
-                        </div>
-                    </>
-                ))}
+                <SearchContainer>
+                    <input
+                        placeholder="Search"
+                        onChange={(e) => setIdtext(e.target.value)}
+                    />
+                    <SearchIcon>
+                        <AiOutlineSearch />
+                    </SearchIcon>
+                </SearchContainer>
+                <Image>
+                    <img
+                        src={`https://myceliademo.blob.core.windows.net/fashion-imgs/images/${idtext}.jpg`}
+                        alt="product"
+                    />
+                </Image>
             </ImageConatiner>
+            <SemelantyContainer>
+                <h1>Produtos similares</h1>
+                <ScrollConatiner>
+                    {results && (
+                        <>
+                            {results.map(({ id, distance }) => {
+                                return (
+                                    <ContainerItems>
+                                        <ImagemItem>
+                                            <img
+                                                src={`https://myceliademo.blob.core.windows.net/fashion-imgs/images/${id}.jpg`}
+                                                alt="product"
+                                            />
+                                        </ImagemItem>
+                                    </ContainerItems>
+                                );
+                            })}
+                        </>
+                    )}
+                </ScrollConatiner>
+            </SemelantyContainer>
         </ProductsContainer>
     );
 };
--- src/pages/Products/styles.jsx
+++ src/pages/Products/styles.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { btnReset, v } from "../../styles/variables";
 
@@ -14,12 +13,120 @@ export const ImageConatiner = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;    
-    justify-content: center;
+    justify-content: space-between;
     height: 80vh;
-
+    margin-right: ${v.xlSpacing};
     border-radius: ${v.borderRadius};
-    padding: ${v.xxlSpacing};
+    padding: ${v.smSpacing} ${v.xlSpacing};
     width: 100%;
     background: ${({ theme }) => theme.bg};
     box-shadow: 5px 5px 5px ${({ theme }) => theme.bg3}, 0 0 10px ${({ theme }) => theme.bg};
+`;
+
+
+export const SearchContainer = styled.div`
+    background: ${({ theme }) => theme.bgAlpha};
+    border: 1px solid ${({ theme }) => theme.bg3};
+    width: 100%;
+    border-radius: ${v.borderRadius};
+    
+
+    input {
+        padding: 0 ${v.smSpacing};
+        font-family: inherit;
+        letter-spacing: inherit;
+        font-size: 16px;
+        width: 100%;
+        outline: none;
+        border: none;
+        color: inherit;
+        background: transparent;
+        transition: all 0.2s ease-in-out;
+        &:focus {
+            border: 1px solid ${({ theme }) => theme.primary};
+        border-radius: ${v.borderRadius} 0 0 ${v.borderRadius};    
+        }
+    }
+    display: flex;
+`;
+
+export const SearchIcon = styled.button`
+    ${btnReset};
+    padding: calc(${v.mdSpacing} - 2px) ${v.mdSpacing};
+    display: flex;
+    cursor: pointer;
+    svg {
+        font-size: 20px;
+    }
+`;
+
+
+export const Image = styled.div`
+    height: 68vh;
+    width: 50vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.bgAlpha};
+    border-radius: ${v.borderRadius};
+    border: 1px solid ${({ theme }) => theme.bg3};
+    img {
+        max-width: 100%;
+        height: 100%;
+        border-radius: ${v.borderRadius};
+    }
+`;
+
+export const SemelantyContainer = styled.div`
+    height: 80vh;
+    border-radius: ${v.borderRadius};
+    padding: ${v.xxlSpacing};
+    width: 90%;
+    background: ${({ theme }) => theme.bg};
+    box-shadow: 5px 5px 5px ${({ theme }) => theme.bg3}, 0 0 10px ${({ theme }) => theme.bg};
+
+    h1{
+        text-align: center;
+        font-size: 25px;
+        font-weight: bold;
+    }
+`;
+
+export const ScrollConatiner = styled.div`
+display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(130px, 1fr));
+    grid-gap: ${v.smSpacing};
+    overflow-y: scroll;
+    width: 100%;
+    height: 100%;
+`;
+
+export const ContainerItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 30vh;
+    width: 22vh;
+    border-radius: ${v.borderRadius};
+    background: ${({ theme }) => theme.bgAlpha};
+    border: 1px solid ${({ theme }) => theme.bg3};
+`;
+
+export const ImagemItem = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30vh;
+    padding: ${v.smSpacing};
+    img {
+        z-index: 1;
+        width: 100%;
+        height: 100%;
+        border-radius: ${v.borderRadius};
+
+        &:hover {
+            cursor: pointer;
+            transform: scale(1.09);
+        }
+    }
 `;
